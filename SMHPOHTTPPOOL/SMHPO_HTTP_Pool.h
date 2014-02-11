b8266a1ae5087ba59e0b46038d319f537019eb64
gsmartrookie <gsmartrookie@gmail.com>
2014-02-11 18:26:50 +0800

    注册首次走通。

--- SMHPOHTTPPOOL/SMHPO_HTTP_Pool.h
+++ SMHPOHTTPPOOL/SMHPO_HTTP_Pool.h
@@ -14,8 +14,25 @@ typedef void (^failure)(id error);
 
 @interface SMHPO_HTTP_Pool : NSObject
 
-
+/**
+ *  获取验证码
+ *
+ *  @param phone   电话
+ *  @param success 成功返回值
+ *  @param failure 失败返回值
+ */
 + (void)SMHPO_verify_code_PhoneNum:(NSString *)phone withSuccess:(success)success withFailure:(failure)failure;
 
+/**
+ *  客户端通过手机注册帐号
+ *
+ *  @param name        姓名
+ *  @param pwd         密码
+ *  @param phone       电话
+ *  @param verify_code 验证码
+ *  @param success     成功返回值
+ *  @param failure     失败返回值
+ */
++ (void)SMHPO_signup_Name:(NSString *)name withPassword:(NSString *)pwd withPhone:(NSString *)phone withVerify_code:(NSString *)verify_code withSuccess:(success)success withFailure:(failure)failure;
 
 @end
--- SMHPOHTTPPOOL/SMHPO_HTTP_Pool.m
+++ SMHPOHTTPPOOL/SMHPO_HTTP_Pool.m
@@ -16,7 +16,28 @@
 {
     NSString *urlStr = [NSString stringWithFormat:@"http://115.28.3.92:8000/verify_code?phone=%@&session_id=123",phone];
     NSURL *url = [NSURL URLWithString:urlStr];
-    NSURLRequest *urlRequest = [NSURLRequest requestWithURL:url];
+    NSMutableURLRequest *urlRequest = [NSMutableURLRequest requestWithURL:url];
+    [urlRequest addValue:@"XMLHttpRequest" forHTTPHeaderField:@"X-Requested-With"];
+    AFHTTPRequestOperation *httpRequest = [[AFHTTPRequestOperation alloc] initWithRequest:urlRequest];
+    [httpRequest setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
+        NSDictionary *respDic = [NSJSONSerialization JSONObjectWithData:responseObject options:NSJSONReadingAllowFragments error:Nil];
+        if (success) {
+            success(respDic);
+        }
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        if (failure) {
+            failure(error);
+        }
+    }];
+    [httpRequest start];
+}
+
++ (void)SMHPO_signup_Name:(NSString *)name withPassword:(NSString *)pwd withPhone:(NSString *)phone withVerify_code:(NSString *)verify_code withSuccess:(success)success withFailure:(failure)failure
+{
+    NSString *urlStr = [NSString stringWithFormat:@"http://115.28.3.92:8000/signup?password=%@&&verify_code=%@",pwd,verify_code];
+    NSURL *url = [NSURL URLWithString:urlStr];
+    NSMutableURLRequest *urlRequest = [NSMutableURLRequest requestWithURL:url];
+    [urlRequest addValue:@"XMLHttpRequest" forHTTPHeaderField:@"X-Requested-With"];
     AFHTTPRequestOperation *httpRequest = [[AFHTTPRequestOperation alloc] initWithRequest:urlRequest];
     [httpRequest setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
         NSDictionary *respDic = [NSJSONSerialization JSONObjectWithData:responseObject options:NSJSONReadingAllowFragments error:Nil];
